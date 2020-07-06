eb17ee30545302099623e22a5b62461599b9ea00
Kayla Wiggins <wiggikay@umich.edu>
2020-07-05 20:16:18 -0400

    2020 website overhaul

--- motor.js
+++ motor.js
@@ -1,8 +1,44 @@
-$(document).ready(function(){
-  
-ScrollReveal().reveal('p',{ delay: 400 });
-ScrollReveal().reveal('img',{ delay: 400 });
-ScrollReveal().reveal('.icon',{ delay: 400 });
+
+$(document).ready(function() {
+
+  // Create a timeline with default parameters
+  var tl = anime.timeline({
+    targets: '.card',
+    delay: 300,
+  });
+  // Add children
+  tl
+    .add({
+      targets: '.card',
+      opacity: 1,
+      easing: 'linear',
+      delay: anime.stagger(700)
+    })
+    .add({
+      targets: '.card',
+        opacity: 0,
+        easing: 'linear',
+        duration: 2000
+      }
+      ,'+=2000')
+      .add({
+        targets: '#nameside',
+          opacity: 1,
+          easing: 'linear',
+          duration: 3000
+        }
+        ,'-=1')
+
+        ///////////////////////////////////
+
+        var animation = anime({
+        targets: '.plus',
+        rotate: 360,
+      });
+
+      document.querySelector('.plus').onmouseover = animation.play;
+
+      //////////////////////////////////////
 
 
 });
